53571705dfa3228b45e625863f49ad9ca9011f52
MaksPlank <6677limb@gmail.com>
2020-09-25 19:22:34 +0300

    Initial Commit

---
+++ GetOrPost/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetOrPost
+//
+//  Created by Maks Plank on 25.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
